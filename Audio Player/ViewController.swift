3dd27d9c80508a9600b9461cf6ec386193ebf3bd
ITP <itp@172-16-251-23.dynapool.nyu.edu>
2017-10-11 18:35:10 -0400

    Adding the code for problem set 2

--- Audio Player/ViewController.swift
+++ Audio Player/ViewController.swift
@@ -12,22 +12,47 @@ import AVFoundation
 class ViewController: UIViewController {
     
     //Instantiate the AVFoundation audio player class
-    var player: AVAudioPlayer = AVAudioPlayer()
+    var player: AVAudioPlayer? = nil
+    
+    //Timer for tracking the progress
+    var timer: Timer? = nil
+    
+    //References to the UI elements
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var ProgressLabel: UILabel!
+    @IBOutlet weak var ProgressBar: UIProgressView!
+    
+    let volume = AVAudioSession.sharedInstance().outputVolume
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        volumeSlider.value = volume
+        
+        
         do {
             //Set the path to the audio file (comes from the bundle)
-            let pathToSong = Bundle.main.path(forResource: "song", ofType: "mp3")
+            let path = Bundle.main.url(forResource: "song", withExtension: "mp3")
             
-
-            //Try instantiating the audio player using the path to the song
-            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: pathToSong!) as URL)
-
+            //Unpacking the path string optional
+            if let unpackedPath = path {
+                
+                try player = AVAudioPlayer(contentsOf: unpackedPath)
+                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                    
+                    self.ProgressBar.progress = Float(self.player!.currentTime / self.player!.duration)
+                    self.ProgressLabel.text = String(format: "%d:%02d", Int(self.player!.currentTime) / 60, Int(self.player!.currentTime) % 60)
+                }
+                player!.play()
+                timer!.fire()
+                
+            }
+            
         } catch {
-
+            print(error)
         }
+        
+        print("[Setup] Audio Player created")
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,10 +60,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func PressPlay(_ sender: Any) {
-        player.play()
+        player!.play()
     }
-
+    
+    @IBAction func PressStop(_ sender: Any) {
+        player!.stop()
+        player!.currentTime = 0
+    }
+    
+    @IBAction func PressPause(_ sender: Any) {
+        player!.pause()
+    }
+    
+    @IBAction func VolumeChange(_ sender: Any) {
+        player!.volume = volumeSlider.value
+    }
 }
 
